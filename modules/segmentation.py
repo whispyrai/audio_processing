bc1e54569ad1124f3aa5af334cde31ba9eca802c
Hesham El-Mahdi <heshamelmahdi@gmail.com>
2025-04-16 13:36:13 -0400

    move segmentation code into modules folder

--- modules/segmentation.py
+++ modules/segmentation.py
@@ -1,7 +1,8 @@
 from pydub import AudioSegment, silence
 import os
 
-def segment_audio(audio_path, min_silence_len=1000, silence_thresh=-40, skip_initial=10000):
+
+def segment_audio(audio_path, min_silence_len=1000, silence_thresh=-40, skip_initial=0):
     audio = AudioSegment.from_file(audio_path)
 
     # Skip the first 10 seconds
@@ -12,14 +13,14 @@ def segment_audio(audio_path, min_silence_len=1000, silence_thresh=-40, skip_ini
         audio,
         min_silence_len=min_silence_len,
         silence_thresh=silence_thresh,
-        keep_silence=300  # preserves prosodic features
+        keep_silence=300,  # preserves prosodic features
     )
 
     # Extract the directory of the input file
     input_dir = os.path.dirname(audio_path)
 
-    # Define a subdirectory called "segmented_chunks"
-    output_dir = os.path.join(input_dir, "segmented_chunks")
+    # Define a subdirectory called "segmented-chunks"
+    output_dir = os.path.join(input_dir, "segmented-chunks")
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -32,7 +33,3 @@ def segment_audio(audio_path, min_silence_len=1000, silence_thresh=-40, skip_ini
         chunk.export(output_chunk_path, format="wav")
 
     print(f" Segmentation complete: {len(chunks)} chunks saved in '{output_dir}'.")
-
-# Example usage
-audio_path = r"C:\Users\nadee\Downloads\Download (audio-extractor.net).mp3"
-segment_audio(audio_path, min_silence_len=500, silence_thresh=-40, skip_initial=80000) 
